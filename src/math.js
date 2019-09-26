cb9fdb63a4c2aaf36c894032a5e3eb8cae3882d8
mehdi-tahri <mehdi.tahri07@gmail.com>
2019-09-26 15:11:09 +0200

    first commit

---
+++ src/math.js
@@ -0,0 +1,137 @@
+let Util = {};
+Util.factorial = (n) => {
+    if (n === 0) {
+        return 1;
+    }
+
+    if (n >= 3000) {
+        throw 'n too large'
+    }
+
+    if (n < 0) {
+        throw 'n is negative'
+    }
+
+    return n * Util.factorial(n - 1);
+};
+
+/**
+ * Détermine si n est un nombre premier.
+ * Util.isPrime(5) => false
+ * Util.isPrime(6) => true
+ *
+ * @param {number} n
+ * @returns {boolean}
+ */
+Util.isPrime = function (n) {
+    if (n === 1 || n === 0) {
+        return false;
+    }
+    if (n < 0) {
+        throw 'Unable to compute prime for n < 0'
+    }
+    for (var i = 2; i < n; i++)
+        if (n % i === 0) return false;
+    return true;
+
+};
+
+
+/**
+ * Additionne l'ensemble des nombres premiers de 2 à n
+ *
+ * Util.sumPrime(6) = 2 + 3 + 5 = 10
+ * Util.sumPrime(8) = 2 + 3 + 5 + 7 = 17
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+Util.sumPrime = function(n) {
+	var res=0;	
+	if(typeof n !=typeof 1) throw 'Unable to compute sumPrime for n not number (int)';
+	if(n>=2){
+		for(var i=2; i<=n;i++){
+			if(Util.isPrime(i)){
+				res+=i;			
+			}
+		}
+	}
+	if(n<0){
+		throw 'Unable to compute sumPrime for n < 0'	
+	}
+	return res;
+};
+
+/**
+ * Cette méthode doit retourner un tableau de 1 à n tel que:
+ * - Pour les nombres multiples de 3, les remplacer par "Fizz"
+ * - Pour les nombres multiples de 5, les remplacer par "Buzz"
+ * - Pour les nombres multiples de 3 et 5, les remplacer par "FizzBuzz"
+ *
+ * Exp :
+ * Util.fizzBuzz(15) => [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+ *
+ * @param {number} n
+ * @returns {array}
+ */
+Util.fizzBuzz = function(n) {
+	var tabRes=[];
+	if(typeof n !=typeof 1) throw 'Unable to compute fizzBuzz for n not number (int)';
+	if(n<1){
+		throw 'Unable to compute fizzBuzz for n < 1';	
+	}
+	for (var i=1; i <= n; i++){
+    		if (i % 15 == 0) tabRes.push('FizzBuzz');
+    		else if (i % 3 == 0) tabRes.push("Fizz");
+    		else if (i % 5 == 0) tabRes.push("Buzz");
+    		else tabRes.push(i);
+	}
+	return tabRes;
+};
+
+/**
+ * Chiffre une phrase selon la règle suivante : Les A deviennent des B, les B des C, etc.
+ *
+ * Exp :
+ * Util.cipher("Test Unitaire") => "Uftu Tojubjsf"
+ *
+ * @param phrase
+ * @returns {string}
+ */
+Util.cipher = function (phrase) {
+	if(typeof phrase !='string') throw 'Unable to compute cipher for phrase not string';	
+	var res ="";
+	for(let i=0; i< phrase.length;i++){
+		if(phrase[i] !=" ") res+=String.fromCharCode(phrase[i].charCodeAt(0) + 1);
+		else res+=" ";
+	}
+	return res;
+};
+
+/**
+ * Retourne le nombre de paires dans un tableau
+ *
+ * Exp :
+ *
+ * Util.pairs([3,3]) => 1
+ * Util.pairs([3,3,5,]) => 1
+ * Util.pairs([3,3,5,5,5]) => 4
+ *
+ * @param array
+ * @return int
+ */
+Util.pairs = function(array) {
+	let res=0;
+	for(let i=0; i<array.length;i++){
+		let variable=array[i];	
+		for(let j=i+1; j<array.length;j++){
+			if(variable==array[j]){
+				res++;			
+			}
+		}	
+	}
+	return res;
+};
+
+module.exports = Util;
+
---
+++ src/math.test.js
@@ -0,0 +1,130 @@
+const Util = require('./math');
+test('Test factoriel de 0 => 1', () => {
+    expect(Util.factorial(0)).toBe(1);
+});
+
+test('Test factoriel de 2 => 2', () => {
+    expect(Util.factorial(3)).toBe(6);
+});
+
+test('Test factoriel de 3 => 6', () => {
+    expect(Util.factorial(3)).toBe(6);
+});
+
+test('Test factoriel de 3000', () => {
+    expect(()=> {Util.factorial(3000)}).toThrow();
+});
+
+test('Test factoriel -10', () => {
+    expect(()=> {Util.factorial(-10)}).toThrow(/negative/);
+});
+
+
+describe('isPrime', function () {
+
+    test('Test prime de 1 => false', () => {
+        expect(Util.isPrime(1)).toBe(false)
+    });
+    test('Test prime de 0 => false', () => {
+        expect(Util.isPrime(0)).toBe(false)
+    });
+    test('Test prime < 0 => throw exception', () => {
+        expect(() => { Util.isPrime(-10) }).toThrow('Unable to compute prime for n < 0');
+    });
+
+    test.each([
+        [2, true],
+        [5, true],
+        [17, true],
+        [18, false],
+        [53, true],
+        [55, false],
+    ])(
+        'isPrime %i equals to %i',
+        (n, expected) => {
+            expect(Util.isPrime(n)).toBe(expected);
+        }
+    );
+});
+
+describe('sumPrime', function () {
+
+test('Test sumPrime < 0 => throw exception', () => {
+        expect(() => { Util.sumPrime(-10) }).toThrow('Unable to compute sumPrime for n < 0');
+    });
+test('Test sumPrime not int throw exception', () => {
+        expect(() => { Util.sumPrime("salut") }).toThrow('Unable to compute sumPrime for n not number (int)');
+    });
+test.each([
+        [2, 2],
+        [5, 10],
+        [6, 10],
+        [8, 17],
+    ])(
+        'sumPrime %i equals to %i',
+        (n, expected) => {
+            expect(Util.sumPrime(n)).toBe(expected);
+        }
+    );
+
+});
+
+
+describe('fizzBuzz', function () {
+
+test('Test fizzBuzz < 0 => throw exception', () => {
+        expect(() => { Util.fizzBuzz(-10) }).toThrow('Unable to compute fizzBuzz for n < 1');
+    });
+test('Test fizzBuzz not int throw exception', () => {
+        expect(() => { Util.fizzBuzz("salut") }).toThrow('Unable to compute fizzBuzz for n not number (int)');
+    });
+
+test.each([
+        [5, [1, 2, "Fizz", 4, "Buzz"]],
+        [15, [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]],
+    ])(
+        'fizzBuzz %i equals to %i',
+        (n, expected) => {
+            expect(Util.fizzBuzz(n)).toStrictEqual(expected);
+        }
+    );
+
+});
+
+describe('cipher', function () {
+
+test('Test cipher phrase not string (int) throw exception', () => {
+        expect(() => { Util.cipher(1) }).toThrow('Unable to compute cipher for phrase not string');
+    });
+
+test('Test cipher phrase not string (empty) throw exception', () => {
+        expect(() => { Util.cipher() }).toThrow('Unable to compute cipher for phrase not string');
+    });
+
+test.each([
+        ["Test Unitaire", "Uftu Vojubjsf"],
+        ["non","opo"],
+    ])(
+        'cipher "%s" equals to "%s"',
+        (n, expected) => {
+            expect(Util.cipher(n)).toStrictEqual(expected);
+        }
+    );
+
+});
+
+describe('pairs', function () {
+
+test.each([
+        [[3,3], 1],
+        [[3,3,5,],1],
+	[[3,3,5,5,5],4],
+    ])(
+        'pairs %d equals to %i',
+        (n, expected) => {
+            expect(Util.pairs(n)).toStrictEqual(expected);
+        }
+    );
+
+});
+
